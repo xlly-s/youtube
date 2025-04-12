eaf63e7393e1c0ff5e5a761f2433943256330bce
xlly-s <137001490+xlly-s@users.noreply.github.com>
2025-04-12 20:41:26 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.json['url']
+    ydl_opts = {'quiet': True, 'skip_download': True}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return jsonify({
+            'title': info['title'],
+            'url': info['url'] if 'url' in info else info['webpage_url']
+        })
+
+if __name__ == '__main__':
+    app.run()
